d9f352b80ba2689b35862b9a77c8b86d1c79ddd0
Niteshspandey1 <niteshspandey2511@gmail.com>
2024-03-16 22:46:35 +0530

    adding js file

---
+++ array1class.js
@@ -0,0 +1,50 @@
+// // array method
+// // adding of item in array
+//  let ar=[1,2,3,4,5,6,7,8,9,"shubham", false];
+//  ar.push(6,7);
+//  console.log(ar);
+
+// // remove item from array
+// let arr=["ram","shyam","yudhya",1,2,3,]
+// arr.pop();
+// console.log(arr);
+
+
+
+// // for remove 1st item from array
+// let arr=["ram","shyam","yudhya",1,2,3,]
+// arr.shift();
+// console.log(arr);\
+
+// // for return 1st item from array
+// let arr=["ram","shyam","yudhya",1,2,3,]
+// arr.unshift();
+// console.log(arr);
+
+// // for adding two array
+// let a1=[1,2,3,4,5,6]
+// let a2=[5,6,7,8]
+// let a3=a1.concat(a2);
+// console.log(a3)
+
+// // for creating string from array
+//  a1={1,2,3,4,5,6}
+//  let s=a1.join("");
+// console.log(s);
+
+a=["n", "i","t",'e','s','h']
+// let s=a.join("");
+// console.log(s);
+
+// let a=n,i,t,e,s,h;
+// let i
+// for(i=0;i<6;i++;)
+// {
+  
+//     let s=a
+//     console.log(i);
+
+for(let i=0;i<a.length;i++){
+
+    console.log(a[i])
+}
